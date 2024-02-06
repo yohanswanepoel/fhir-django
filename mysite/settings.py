e42cbfddd453055d1608adaacf1a2829a866c89b
Johan <johannes.swanepoel@gmail.com>
2024-02-07 08:31:32 +1000

    Updated with demo url

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
-FHIR_SERVER_BASE_URL = 'https://fhir-server-fhir-route-demo2.apps.jdemo.sandbox1973.opentlc.com/fhir'
+FHIR_SERVER_BASE_URL = 'https://fhir-server-fhir-route-demo2.apps.cluster-5825p.dynamic.redhatworkshops.io/fhir'
 
 TEMPLATES = [
     {
